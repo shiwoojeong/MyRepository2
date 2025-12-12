f749b01346fa61a9ab379ff1bfe5e990ea51c985
정시우 <@DESKTOP-DS9APUI>
2025-12-12 21:50:32 +0900

    second

--- GIt/src/mygit/A.java
+++ GIt/src/mygit/A.java
@@ -1,5 +1,7 @@
 package mygit;
 
 public class A {
-
+	public static void main(String[] args) {
+	System.out.println("Alice modified A");
+	}
 }
--- GIt/src/mygit/c.java
+++ GIt/src/mygit/c.java
@@ -1,5 +1,7 @@
 package mygit;
 
 public class c {
-
+	public static void printText(String text) {
+		System.out.println("Alice is change in C class");
+	}
 }
