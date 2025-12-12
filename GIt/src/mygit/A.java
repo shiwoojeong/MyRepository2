d7ec40e86a2d569d98d7b28801aef90195a7f3d3
정시우 <@DESKTOP-DS9APUI>
2025-12-12 21:55:50 +0900

    fourth

--- GIt/src/mygit/A.java
+++ GIt/src/mygit/A.java
@@ -1,5 +1,7 @@
 package mygit;
 
 public class A {
-
+	public static int add(int a, int b) {
+		return a + b;
+	}
 }
--- GIt/src/mygit/B.java
+++ GIt/src/mygit/B.java
@@ -1,5 +1,7 @@
 package mygit;
 
 public class B {
-
+	public static int sub(int a, int b) {
+		return a - b;
+	}
 }
